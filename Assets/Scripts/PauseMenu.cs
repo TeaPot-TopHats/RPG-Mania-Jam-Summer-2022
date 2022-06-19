34dfac53bd416ff02664a899cfc04cca6fd865e5
maitham h <maitham_gh98@hotmail.com>
2022-06-19 01:44:54 -0400

    06/19/2022 | Maiz | worked on setting panel UI

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    
+    // Update is called once per frame
+    void Update() {
+
+        /*
+            An if statement that check for the game if paused or not 
+        */
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused) {
+                Resume();
+                LockMouseCursor();
+            } else {
+                Pause();
+                UnlockMouseCursor();
+            }
+
+        }// end of if input
+    }// end of void update 
+
+    /*
+     * Resume method basiclly restart the game and let the time flow normally
+     */
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        GameIsPaused = false;
+    }// end of reusme method
+
+    /*
+     * Pause method basiclly pasues the game and set the time flow to zero.
+     */
+    public void Pause() {
+
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        GameIsPaused = true;
+    }// end of pause method
+
+    void UnlockMouseCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    void LockMouseCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = false;
+    }
+    public void LoadMenu()
+    {
+        Debug.Log("Loading menu...");
+    }// end of LoadMenu method
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+    }
+}// end of Class
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+    void Start()
+    {
+        /*
+         * First we cleared our dropdown resolution
+         * Create a list of string for our resolution options
+         * A for loop that loop through each of our resolution array and for each option
+         * Created a string formated in the following: "width" + "x" + "height" of the resolution
+         * Lastly add the option to our options list
+         * Then add them to our dropdown list
+         * 
+         * To set the resolution to the correct one right away we use an if statement to check resolution wide is equal
+         * To screen current width resolution same for height
+         */
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "hz";
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width &&
+                resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }// end of for loop
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }// end of start
+    public void SetResolutions(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }// end of setResolutions
+    /*
+     * -----------------------------------------------------------------------------
+     */
+    public void setFullScreen(bool Fullscreen)
+    {
+        Screen.fullScreen = Fullscreen;
+    }// end of Fullscreen
+
+    /*
+     * -----------------------------------------------------------------------------
+     */
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }// end of Quality
+
+    /*
+     * -----------------------------------------------------------------------------
+     */
+    public void QuitGame()
+    {
+        Application.Quit();
+    }// end of QuitGame
+
+    /*
+    * -----------------------------------------------------------------------------
+    */
+
+    public AudioMixer mixer;
+
+    public void SetVolumeLevel(float sliderValue)
+    {
+        /*
+         * represent the slider value as a log 10 and then multiply by 20. This will take the min and max value we set up in unity
+         * and represent it is 0 to -80 on a logirthmic scale
+         */
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+    }// end of SetVolumeLevel
+
+    /*
+    * -----------------------------------------------------------------------------
+    */
+
+    public GameObject Panel;
+    public void OpenPanel()
+    {
+        Panel.SetActive(true);
+    }// end of OpenPanel
+
+    /*
+    * -----------------------------------------------------------------------------
+    */
+
+}// end of Setting Class
