d263dc50be9222647c163af3897126e7cbff5d3f
Spencer Childers <schilders@bellarmine.edu>
2022-07-09 16:03:27 -0400

    7/9 | Spencer | Slight changes

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HorizontalTileMovement : MonoBehaviour
+public class MovingPlatform : MonoBehaviour
 {
     [SerializeField] private float leftBound;
     [SerializeField] private float rightBound;
